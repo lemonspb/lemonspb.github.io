ab8509c865619ecdd3b9ceffbdd362f81c2b77f2
misha <you@example.com>
2019-07-21 18:21:32 +0700

    pages

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.portfolio-item__title').forEach((title)=>{
+    title.addEventListener('click', (e)=>{
+            window.open(`${e.target.nextElementSibling.href
+            }`, '_blank');
+    })
+})
