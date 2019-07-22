d2e892ad70d18b7e8dec72fafe1202cfb09a021a
Misha Skopenko <misha.skopenko@yandex.ru>
2019-07-23 03:36:40 +0700

    test-scrile

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ document.querySelectorAll('.portfolio-item__title').forEach((title)=>{
     title.addEventListener('click', (e)=>{
             window.open(`${e.target.nextElementSibling.href
             }`, '_blank');
-    })
-})
+    });
+});
