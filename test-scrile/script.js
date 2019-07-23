c12cce24ece8ce277ee14fdc8038ace012cbdc3d
misha <you@example.com>
2019-07-23 12:50:44 +0700

    init

--- test-scrile/script.js
+++ test-scrile/script.js
@@ -1,49 +1,60 @@
 class Observable {
-    constructor() {
-      this.value = undefined;
-      this.callbacks = [];
-    }
-    onChange(callback) {
-      this.callbacks.push(callback);
-    }
-    set(value) {
-      this.value = value;
-      this.callbacks.forEach(cb => cb(value));
-    }
+  constructor() {
+    this.value = undefined;
+    this.callbacks = [];
   }
-
-  const store = {
-    count: new Observable(),
-  };
-
-function getCount(){
-         fetch('https://alex.devel.softservice.org/testapi/').then(async response => {
-          if (response.status !== 200) {
-            return;
-          }
-          const data = await response.json();
-         
-          store.count.set(data.balance_usd);
-        });
+  onChange(callback) {
+    this.callbacks.push(callback);
+  }
+  set(value) {
+    this.value = value;
+    this.callbacks.forEach(cb => cb(value));
+  }
 }
 
-function outputCount(){
-store.count.onChange((count)=>{
-    let width = 8;
-   const countProgress = count*width;
-   const value = document.querySelector(".progress-bar__value");   
-   const interval = setInterval(progress, 1);
-   function progress() {
-     if (width >= countProgress) {
-       clearInterval(interval);
-     } else {
-       width++; 
-       value.style.width = width + 'px'; 
-       }
-     }
-   
-
-}); 
+const store = {
+  balance: new Observable()
 };
+
+function getCount() {
+  fetch("https://alex.devel.softservice.org/testapi/").then(async response => {
+    if (response.status !== 200) {
+      return;
+    }
+    const data = await response.json();
+
+    store.balance.set(data.balance_usd);
+    
+  });
+  
+}
+
+function outputCount() {
+  store.balance.onChange(balance => {
+    document.querySelector(".initial__value").innerText = `$${balance}`;
+    let width = 8;
+    let finalWidth = width/5;
+    console.log(finalWidth)
+    const countProgress = balance * width + "px";
+    let value = document.querySelector(".progress-bar__value");
+    value.style.width = countProgress;
+    var id = setInterval(frame, 2000);
+
+// if(value.style.width == 120 + 'px'){
+//   document.querySelector('.target').style.background  = '#00A910';
+//   document.querySelector('.reach').style.opacity = '0';
+// }
+    function frame() {
+      if (value.style.width >= 120) {
+        clearInterval(id);
+      } else {
+        finalWidth++; 
+        value.style.width = countProgress +  finalWidth + 'px';
+      }
+    }
+
+  });
+}
 outputCount();
 getCount();
+
