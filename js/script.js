ae9878c5a483fbcb4ac1c8854baa4de3d75c8e0f
misha <you@example.com>
2019-07-27 21:39:59 +0700

    dfd

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-document.querySelectorAll('.portfolio-item__title').forEach((title)=>{
-    title.addEventListener('click', (e)=>{
-            window.open(`${e.target.nextElementSibling.href
-            }`, '_blank');
-    });
+document.querySelectorAll(".portfolio-item__title").forEach(title => {
+  title.addEventListener("click", e => {
+    window.open(`${e.target.nextElementSibling.href}`, "_blank");
+  });
 });
+
+function scrollNavBlock() {
+  window.addEventListener("scroll", () => {
+    document.body.scrollTop > 0 || document.documentElement.scrollTop > 0
+      ? document.querySelector(".page-nav").classList.add("scroll-nav")
+      : document.querySelector(".page-nav").classList.remove("scroll-nav");
+  });
+}
+function NavMobile(){
+    document.querySelector('.burger-menu').addEventListener('click', ()=>{
+const  navLost = document.querySelector('.page-nav__list')
+navLost.classList.toggle('flex');
+
+
+    });
+
+}
+NavMobile()
+scrollNavBlock();
