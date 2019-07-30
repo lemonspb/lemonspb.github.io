e7213c98dc6b76923411a7dbe53453bdbd58b0f2
Misha Skopenko <misha.skopenko@yandex.ru>
2019-07-30 21:55:36 +0700

    fdf

--- layouts/test-sibers/script.js
+++ layouts/test-sibers/script.js
@@ -14,7 +14,7 @@ class Observable {
 const store = {
   contats: new Observable()
 };
-fetch(`http://demo.sibers.com/users`).then(async response => {
+fetch(`https://demo.sibers.com/users`).then(async response => {
   if (response.status !== 200) {
     return;
   }
@@ -24,37 +24,34 @@ fetch(`http://demo.sibers.com/users`).then(async response => {
 });
 
 store.contats.onChange(contacts => {
-   console.log(contacts)
-  contacts.forEach(({ name,avatar , phone, address, website}) => {
-    const contactBlock = document.createElement('div');
-    contactBlock.classList.add('contact');
-const  nameContact =  document.createElement('div');
-const  avatarContact =  document.createElement('img');
-avatarContact.classList.add('contact__img');
-avatarContact.setAttribute('src' , avatar);
+  console.log(contacts);
+  contacts.forEach(({ name, avatar, phone, address, website }) => {
+    const contactBlock = document.createElement("div");
+    contactBlock.classList.add("contact");
+    const nameContact = document.createElement("div");
+    const avatarContact = document.createElement("img");
+    avatarContact.classList.add("contact__img");
+    avatarContact.setAttribute("src", avatar);
 
-nameContact.classList.add('contact__name');
-nameContact.innerText = name;
-contactBlock.appendChild(avatarContact)
-contactBlock.appendChild(nameContact)
-document.querySelector('.container').appendChild(contactBlock)
-contactBlock.addEventListener('click', (e)=>{
-    document.getElementById("myModal").style.display = "block";
-    document.querySelector('.modal__name').innerText = name;
-    document.querySelector('.modal__phone').innerText = phone;
-    document.querySelector('.modal__city').innerText = address.city;
-    document.querySelector('.modal__website').innerText = website;
-    document.querySelector('.modal__country').innerText = address.country;
-    document.querySelector('.modal__img').src = avatar;
-
-})
+    nameContact.classList.add("contact__name");
+    nameContact.innerText = name;
+    contactBlock.appendChild(avatarContact);
+    contactBlock.appendChild(nameContact);
+    document.querySelector(".container").appendChild(contactBlock);
+    contactBlock.addEventListener("click", e => {
+      document.getElementById("myModal").style.display = "block";
+      document.querySelector(".modal__name").innerText = name;
+      document.querySelector(".modal__phone").innerText = phone;
+      document.querySelector(".modal__city").innerText = address.city;
+      document.querySelector(".modal__website").innerText = website;
+      document.querySelector(".modal__country").innerText = address.country;
+      document.querySelector(".modal__img").src = avatar;
+    });
   });
 });
 
-
-
 window.onclick = function(event) {
-    if (event.target == document.getElementById("myModal")) {
-        document.getElementById("myModal").style.display = "none";
-    }
+  if (event.target == document.getElementById("myModal")) {
+    document.getElementById("myModal").style.display = "none";
   }
+};
