8506f43714e48853ff91520647d3744a78eedfa7
Misha Skopenko <misha.skopenko@yandex.ru>
2019-08-05 20:00:39 +0700

    layout-2

--- layouts/test-sibers/script.js
+++ layouts/test-sibers/script.js
@@ -14,7 +14,7 @@ class Observable {
 const store = {
   contats: new Observable()
 };
-fetch(`https://demo.sibers.com/users`).then(async response => {
+fetch(`http://demo.sibers.com/users`).then(async response => {
   if (response.status !== 200) {
     return;
   }
@@ -25,7 +25,7 @@ fetch(`https://demo.sibers.com/users`).then(async response => {
 
 store.contats.onChange(contacts => {
   console.log(contacts);
-  contacts.forEach(({ name, avatar, phone, address, website }) => {
+  contacts.forEach(({ id,name, avatar, phone, address, website, email }) => {
     const contactBlock = document.createElement("div");
     contactBlock.classList.add("contact");
     const nameContact = document.createElement("div");
@@ -40,18 +40,58 @@ store.contats.onChange(contacts => {
     document.querySelector(".container").appendChild(contactBlock);
     contactBlock.addEventListener("click", e => {
       document.getElementById("myModal").style.display = "block";
-      document.querySelector(".modal__name").innerText = name;
-      document.querySelector(".modal__phone").innerText = phone;
-      document.querySelector(".modal__city").innerText = address.city;
-      document.querySelector(".modal__website").innerText = website;
-      document.querySelector(".modal__country").innerText = address.country;
-      document.querySelector(".modal__img").src = avatar;
-    });
-  });
-});
+     document.querySelector(".modal__name").innerText = name;
+        document.querySelector(".modal__phone").innerText = phone;
+         document.querySelector(".modal__city").innerText = address.city;
+         document.querySelector(".modal__website").innerText = website;
+        document.querySelector(".modal__country").innerText = address.country;
+                               document.querySelector(".modal__img").src = avatar;
+          document.querySelector(".modal__email").innerText = email;
+ 
 
-window.onclick = function(event) {
-  if (event.target == document.getElementById("myModal")) {
-    document.getElementById("myModal").style.display = "none";
+document.querySelector(".edit").addEventListener("click", () => {
+    document.querySelectorAll('.val').forEach( (val)=>{
+        val.setAttribute("contenteditable", true);
+    
+    });
+    document.querySelector(".edit").style.display = 'none'; 
+    document.querySelector(".save").style.display = 'block';
+    
+    });
+    document.querySelector('.save').addEventListener('click', ()=>{
+        document.querySelectorAll('.val').forEach( (val)=>{
+            val.setAttribute("contenteditable", false);
+    });
+    document.querySelector(".edit").style.display = 'block';
+    document.querySelector(".save").style.display = 'none'; 
+    console.log( document.querySelector(".modal__website").innerText)
+  changeContacts(id,document.querySelector(".modal__name").innerText, avatar,  ContactPhone , address, ContactWebsite, Contactmail);
+    });
+    closeModal();
+});
+});
+});
+function closeModal() {
+  const modal = document.getElementById("myModal");
+  window.addEventListener("click", e => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+      document.querySelector(".edit").style.display = 'block'; 
+
+    }
+  });
+  document.querySelector(".close").addEventListener("click", () => {
+    modal.style.display = "none";
+    document.querySelector(".edit").style.display = 'block'; 
+
+  });
+
+}
+
+
+
+function changeContacts(id,name, avatar, phone, address, website, email) {
+    const contacts = JSON.parse(localStorage.getItem("storageContacts")) || {};
+    contacts[id] = [name, avatar, phone, address, website, email];
+    localStorage.setItem("storageContacts", JSON.stringify(contacts));
   }
-};
