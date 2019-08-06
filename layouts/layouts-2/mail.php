4df3b1749ec3f55d44ad12d9c8b51dbc00ea9b69
Misha Skopenko <misha.skopenko@yandex.ru>
2019-08-06 20:29:38 +0700

    df

--- layouts/layouts-2/mail.php
+++ layouts/layouts-2/mail.php
@@ -10,7 +10,7 @@ $email = trim($email);
 echo $name;
 //echo "<br>";
 //echo $email;
-if (mail("misha.skopenko@yandex.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+if (mail("misha.skopenko@yandex.ru", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: misha.skopenko@yandex.ru \r\n"))
  {     echo "сообщение успешно отправлено"; 
 } else { 
     echo "при отправке сообщения возникли ошибки";
